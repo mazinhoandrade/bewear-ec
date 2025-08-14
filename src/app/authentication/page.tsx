856f5b4be89438dbbc4e2a5f535b2b53622ea437
mazinhoandrade <irismarandrade94@gmail.com>
2025-08-14 15:07:13 -0300

    feat: add sign out

--- src/app/authentication/page.tsx
+++ src/app/authentication/page.tsx
@@ -1,25 +1,30 @@
 import React from "react";
 
+import Header from "@/components/common/header";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import SignIn from "./components/sign-in";
 import SignUp from "./components/sign-up";
 const Authentication = () => {
   return (
-    <div className="flex w-full max-w-md flex-col gap-6 p-5 m-auto ">
-      <Tabs defaultValue="sign-in">
-        <TabsList>
-          <TabsTrigger value="sign-in">Entrar</TabsTrigger>
-          <TabsTrigger value="sign-up">Criar Conta</TabsTrigger>
-        </TabsList>
-        <TabsContent value="sign-in">
-          <SignIn />
-        </TabsContent>
-        <TabsContent value="sign-up">
-          <SignUp />
-        </TabsContent>
-      </Tabs>
-    </div>
+    <>
+      <Header />
+
+      <div className="flex w-full max-w-md flex-col gap-6 p-5 m-auto ">
+        <Tabs defaultValue="sign-in">
+          <TabsList>
+            <TabsTrigger value="sign-in">Entrar</TabsTrigger>
+            <TabsTrigger value="sign-up">Criar Conta</TabsTrigger>
+          </TabsList>
+          <TabsContent value="sign-in">
+            <SignIn />
+          </TabsContent>
+          <TabsContent value="sign-up">
+            <SignUp />
+          </TabsContent>
+        </Tabs>
+      </div>
+    </>
   );
 };
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import Header from "@/components/common/header";
+
 export default function Home() {
-  return <div>olá mundo</div>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
