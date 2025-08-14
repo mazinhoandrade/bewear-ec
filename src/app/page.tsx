e7d274ff1f21429a878afea4c9523eff8a9b1f09
mazinhoandrade <irismarandrade94@gmail.com>
2025-08-14 15:45:28 -0300

    feat: add category selector

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,53 @@
-import Header from "@/components/common/header";
+import Image from "next/image";
 
-export default function Home() {
+import CategorySelector from "@/components/common/category-selector";
+import Header from "@/components/common/header";
+import ProductList from "@/components/common/product-list";
+import { db } from "@/db";
+
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
+  const categories = await db.query.categoryTable.findMany();
+
   return (
     <>
       <Header />
+
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner-01.png"
+            alt="Leve um vida com estilo"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full h-auto"
+          />
+        </div>
+        <ProductList title="Mais Vendidos " products={products} />
+
+        <div className="px-5">
+          <CategorySelector categories={categories} />
+        </div>
+
+        <div className="px-5">
+          <Image
+            src="/banner-02.png"
+            alt="Auténtico"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="w-full h-auto"
+          />
+        </div>
+      </div>
     </>
   );
-}
+};
+
+export default Home;
