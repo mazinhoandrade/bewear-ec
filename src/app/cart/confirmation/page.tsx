451c2786f243de4fddde8b1583d9ea04e10a4ae7
mazinhoandrade <irismarandrade94@gmail.com>
2025-08-20 18:31:00 -0300

    feat: ensure order can be created

--- src/app/cart/confirmation/page.tsx
+++ src/app/cart/confirmation/page.tsx
@@ -3,14 +3,13 @@ import { redirect } from "next/navigation";
 
 import Footer from "@/components/common/footer";
 import Header from "@/components/common/header";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { db } from "@/db";
 import { auth } from "@/lib/auth";
 
 import CartSummary from "../components/cart-summary";
 import { formatAddress } from "../helpers/address";
-//import FinishOrderButton from "./components/finish-order-button";
+import FinishOrderButton from "./components/finish-order-button";
 
 const ConfirmationPage = async () => {
   const session = await auth.api.getSession({
@@ -58,7 +57,7 @@ const ConfirmationPage = async () => {
                 <p className="text-sm">{formatAddress(cart.shippingAddress)}</p>
               </CardContent>
             </Card>
-            {/* <FinishOrderButton /> */}
+            <FinishOrderButton />
           </CardContent>
         </Card>
         <CartSummary
