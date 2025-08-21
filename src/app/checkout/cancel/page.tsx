469103c44c25c76db2d9932ec8442a2ed073c824
mazinhoandrade <irismarandrade94@gmail.com>
2025-08-21 19:29:44 -0300

    feat: ensure checkout cancel page

--- src/app/checkout/cancel/page.tsx
+++ src/app/checkout/cancel/page.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+"use client";
+import { Ban } from "lucide-react";
+import Link from "next/link";
+
+import Header from "@/components/common/header";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogTitle,
+} from "@/components/ui/dialog";
 
 const checkoutCancelPage = () => {
-  return <div>checkoutCancelPage</div>;
+  return (
+    <>
+      <Header />
+      <Dialog open={true} onOpenChange={() => {}}>
+        <DialogContent className="text-center">
+          <Ban className="mx-auto" color="red" size={200} />
+          <DialogTitle className="mt-4 text-2xl">Pedido cancelado!</DialogTitle>
+          <DialogDescription className="font-medium">
+            poxa! Seu pedido foi cancelado.
+          </DialogDescription>
+
+          <DialogFooter>
+            <Link href="/my-orders">
+              <Button className="rounded-full" size="lg">
+                Ver meus pedidos
+              </Button>
+            </Link>
+            <Button
+              className="rounded-full"
+              variant="outline"
+              size="lg"
+              asChild
+            >
+              <Link href="/">Voltar para a loja</Link>
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
 };
 
 export default checkoutCancelPage;
